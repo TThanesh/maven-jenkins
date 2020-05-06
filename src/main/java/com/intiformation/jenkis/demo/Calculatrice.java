6735113e47e5e8c65e35e00f94855400107e3a12
THAVARAJAH Thanesh <thavarajah.thanesh@gmail.com>
2020-05-06 09:45:52 +0200

    création du projet

---
+++ src/main/java/com/intiformation/jenkis/demo/Calculatrice.java
@@ -0,0 +1,25 @@
+package com.intiformation.jenkis.demo;
+
+public class Calculatrice {
+
+	/**
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int add(int a , int b) {
+		return a+b;
+	}
+	
+	/**
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int mul(int a, int b) {
+		return a*b;
+	}
+	
+}
---
+++ src/test/java/com/intiformation/jenkis/demo/CalculatriceTest.java
@@ -0,0 +1,26 @@
+package com.intiformation.jenkis.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author IN-DF-017
+ *
+ */
+public class CalculatriceTest {
+
+	@Test
+	public void testAdd() {
+		Calculatrice cal = new Calculatrice();
+		assertEquals("Est ce que l'addition est correcte",10,cal.add(5, 5));
+	}
+
+	@Test
+	public void testMul() {
+		Calculatrice cal = new Calculatrice();
+		assertEquals("Est ce que l'addition est correcte",10,cal.mul(5, 2));
+	}
+
+}
